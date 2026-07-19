98ff49773e2f02c495b4a02221aefc10abb70e84
Higor <cerealistauniaocardoso@gmail.com>
2026-07-18 21:29:41 -0300

    Ajustando porta do servidor para o Render

--- backend/index.js
+++ backend/index.js
@@ -62,6 +62,7 @@ app.post('/login', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
