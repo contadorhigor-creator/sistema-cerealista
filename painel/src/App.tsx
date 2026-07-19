091a52bb5160d923bb3d3152702b1e626b376c0d
Higor <cerealistauniaocardoso@gmail.com>
2026-07-19 13:52:36 -0300

    Corrigindo caminho do import Login

--- painel/src/App.tsx
+++ painel/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './Login';
+import Login from './pages/Login';
 import Dashboard from './Dashboard';
 
 export default function App() {
